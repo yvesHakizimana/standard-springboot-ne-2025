1bf425e67212f5613a08dd458c1e52c9524c7288
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2025-04-30 06:23:55 +0200

    Initializing the repo.

---
+++ src/main/java/com/ne/template/SpringbootNe25TemplateApplication.java
@@ -0,0 +1,13 @@
+package com.ne.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootNe25TemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootNe25TemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ne/template/SpringbootNe25TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ne.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootNe25TemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
